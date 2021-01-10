c1b32e8df71d55543db4e37f4a7be8dbf0ab7262
bikashbist <bikas.bist@yahoo.com>
2021-01-10 12:41:07 +0545

    livewire footer component

--- resources/views/livewire/pos.blade.php
+++ resources/views/livewire/pos.blade.php
@@ -1,3 +1,16 @@
-<div>
 
-</div>
+    <div class="container-fluid">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="d-flex justify-content-center">
+                    <div class="btn__full_width p-2"><button type="button" class="btn btn-primary btn-lg">VISA</button></div>
+                    <div class="btn__full_width p-2"><button type="button" class="btn btn-info btn-lg">IME</button></div>
+                    <div class="btn__full_width p-2"><button type="button" class="btn btn-danger btn-lg">KHALTI</button></div>
+                    <div class="btn__full_width p-2"><button type="button" class="btn btn-success btn-lg">E-SEWA</button></div>
+                    <div class="btn__full_width p-2"><button type="button" class="btn btn-info btn-lg">MASTER CARD</button></div>
+                    <div class="btn__full_width p-2"><button type="button" class="btn btn-primary btn-lg">E-SEWA</button></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
--- resources/views/pos.blade.php
+++ resources/views/pos.blade.php
@@ -37,20 +37,7 @@
         </div>
     </section>
     <section class="footer__part">
-        <div class="container-fluid">
-            <div class="col-md-12">
-                <div class="row">
-                    <div class="d-flex justify-content-center">
-                        <div class="btn__full_width p-2"><button type="button" class="btn btn-primary btn-lg">VISA</button></div>
-                        <div class="btn__full_width p-2"><button type="button" class="btn btn-info btn-lg">IME</button></div>
-                        <div class="btn__full_width p-2"><button type="button" class="btn btn-danger btn-lg">KHALTI</button></div>
-                        <div class="btn__full_width p-2"><button type="button" class="btn btn-success btn-lg">E-SEWA</button></div>
-                        <div class="btn__full_width p-2"><button type="button" class="btn btn-info btn-lg">MASTER CARD</button></div>
-                        <div class="btn__full_width p-2"><button type="button" class="btn btn-primary btn-lg">E-SEWA</button></div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        @livewire('pos')
     </section>
     @livewireScripts
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
