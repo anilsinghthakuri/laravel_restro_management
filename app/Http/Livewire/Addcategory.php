0b77c06276694cf5d2940197cbbd1ef6ce7db7a6
anilsinghthakuri <kinganilcing@gmail.com>
2021-01-19 00:17:00 +0545

    category image problem solve

--- app/Http/Livewire/Addcategory.php
+++ app/Http/Livewire/Addcategory.php
@@ -34,7 +34,7 @@ class Addcategory extends Component
             'category_image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
 
-        $validatedData['category_image'] = $this->category_image->store('public');
+        $validatedData['category_image'] = $this->category_image->store('photo','public');
 
         Category::create($validatedData);
         $this->category = $this->show_category();
