df38e8ffc773ad90669ac55de48ddd878ae4938e
bikashbist <bikas.bist@yahoo.com>
2021-01-18 14:23:58 +0545

    css change

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -5,7 +5,7 @@
 <div class="col-md-9 mt-3">
     <div class="row">
         <div class="col-md-12">
-            <h2 class="add__table__title mb-3 mt-3">Add Tables</h2>
+            <h2 class="add__table__title mb-4 mt-4">Add Tables</h2>
         </div>
 
     </div>
@@ -26,11 +26,12 @@
     <div class="row mt-5">
         <div class="col-md-12">
             <div class="table__list__part ">
-                <table class="table">
+                <table class="table table-responsive">
                     <thead>
                         <tr>
                             <th scope="col">#</th>
                             <th scope="col">Table Name</th>
+                            <th scope="col">Action</th>
 
                         </tr>
                     </thead>
@@ -39,15 +40,19 @@
                             <th scope="row">1</th>
                             <td>Mark</td>
 
+                            <td> <button type="button" class="btn btn-danger"><i class="fa fa-trash-o" aria-hidden="true"></i></button> </td>
+
                         </tr>
                         <tr>
                             <th scope="row">2</th>
                             <td>Jacob</td>
+                            <td> <button type="button" class="btn btn-danger"><i class="fa fa-trash-o" aria-hidden="true"></i></button> </td>
 
                         </tr>
                         <tr>
                             <th scope="row">3</th>
                             <td>@twitter</td>
+                            <td> <button type="button" class="btn btn-danger"><i class="fa fa-trash-o" aria-hidden="true"></i></button> </td>
                         </tr>
                     </tbody>
                 </table>
