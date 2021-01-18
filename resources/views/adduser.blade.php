21ae43cac717e65dfe51fd3a9a34c49e50f72993
bikashbist <bikas.bist@yahoo.com>
2021-01-18 12:19:09 +0545

    addUser

--- resources/views/adduser.blade.php
+++ resources/views/adduser.blade.php
@@ -1,13 +1,209 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>add user</title>
+    <title>Add User</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/categories.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
+    <link rel="stylesheet" href="./style.css">
+    @livewireStyles
+
+
 </head>
+
 <body>
-    <div>
+    <section class="add__categories">
+        <div class="container-fluid">
+
+            <div class="col-md-12">
+
+                <div class="row">
+
+                    <div class="col-md-3 px-0">
+
+                        <nav class="side-menu" id="scrool-bar">
+
+                            <h1 class="side-bar-title">
+                                <ul>
+                                    <li class="d-flex-div"><span>
+                                            <ion-icon name="speedometer-outline"></ion-icon>
+                                        </span> <a class="page-title" href='#message'> Dashbord</a></li>
+                                </ul>
+                            </h1>
+                            <ul>
+
+                                <li class='sub-menu '> <a href='#message'><span>
+                                            <ion-icon name="briefcase-outline"></ion-icon>
+                                        </span> Add Product</a></li>
+                                <li class='sub-menu '> <a href='#settings'> <span>
+                                            <ion-icon name="briefcase-outline"></ion-icon>
+                                        </span> Settings<div class='fa fa-caret-down right'></div></a>
+                                    <ul class="hide">
+                                        <li><a href='#settings'> <span><i class="fa fa-circle-o" aria-hidden="true"></i>
+                                                </span> Account</a></li>
+                                        <li><a href='#settings'> <span><i class="fa fa-circle-o" aria-hidden="true"></i>
+                                                </span> Profile</a></li>
+                                        <li><a href='#settings'> <span><i class="fa fa-circle-o" aria-hidden="true"></i>
+                                                </span> Secruity &amp; Privacy</a></li>
+                                        <li><a href='#settings'> <span><i class="fa fa-circle-o" aria-hidden="true"></i>
+                                                </span> Password</a></li>
+                                        <li><a href='#settings'> <span><i class="fa fa-circle-o" aria-hidden="true"></i>
+                                                </span> Notification</a></li>
+                                    </ul>
+                                </li>
+                                <li class='sub-menu '> <a href='#message'><span>
+                                            <ion-icon name="briefcase-outline"></ion-icon>
+                                        </span> Help<div class='fa fa-caret-down right'></div></a>
+                                    <ul>
+                                        <li><a href='#settings'> <span><i class="fa fa-circle-o" aria-hidden="true"></i>
+                                                </span> FAQ's</a></li>
+                                        <li><a href='#settings'> <span><i class="fa fa-circle-o" aria-hidden="true"></i>
+                                                </span> Submit a Ticket</a></li>
+                                        <li><a href='#settings'> <span><i class="fa fa-circle-o" aria-hidden="true"></i>
+                                                </span> Network Status</a></li>
+                                    </ul>
+                                </li>
+                                <li class='sub-menu '> <a href='#message'><span>
+                                            <ion-icon name="briefcase-outline"></ion-icon>
+                                        </span> Messages</a></li>
+                                <li class='sub-menu '> <a href='#message'><span>
+                                            <ion-icon name="briefcase-outline"></ion-icon>
+                                        </span> Messages</a></li>
+                                <li class='sub-menu '> <a href='#message'><span>
+                                            <ion-icon name="briefcase-outline"></ion-icon>
+                                        </span> Messages</a></li>
+                                <li class='sub-menu '> <a href='#message'><span>
+                                            <ion-icon name="briefcase-outline"></ion-icon>
+                                        </span> Messages</a></li>
+                                <li class='sub-menu '> <a href='#message'><span>
+                                            <ion-icon name="briefcase-outline"></ion-icon>
+                                        </span> Messages</a></li>
+                                <li class='sub-menu '> <a href='#message'><span>
+                                            <ion-icon name="briefcase-outline"></ion-icon>
+                                        </span> Messages</a></li>
+                                <li class='sub-menu '> <a href='#message'><span>
+                                            <ion-icon name="briefcase-outline"></ion-icon>
+                                        </span> Messages</a></li>
+
+                                <li class='sub-menu '> <a href='#message'><span>
+                                            <ion-icon name="briefcase-outline"></ion-icon>
+                                        </span> Messages</a></li>
+                                <li class='sub-menu '> <a href='#message'><span>
+                                            <ion-icon name="briefcase-outline"></ion-icon>
+                                        </span> Messages</a></li>
+                                <li class='sub-menu '> <a href='#message'><span>
+                                            <ion-icon name="briefcase-outline"></ion-icon>
+                                        </span> Messages</a></li>
+                                <li class='sub-menu '> <a href='#message'><span>
+                                            <ion-icon name="briefcase-outline"></ion-icon>
+                                        </span> Messages</a></li>
+                                <li class='sub-menu '> <a href='#message'><span>
+                                            <ion-icon name="briefcase-outline"></ion-icon>
+                                        </span> Messages</a></li>
+                                <li class='sub-menu '> <a href='#message'><span>
+                                            <ion-icon name="briefcase-outline"></ion-icon>
+                                        </span> Messages</a></li>
+                                <li class='sub-menu '> <a href='#message'><span>
+                                            <ion-icon name="briefcase-outline"></ion-icon>
+                                        </span> Messages</a></li>
+
+
+                            </ul>
+                        </nav>
+
+                    </div>
+
+                    <div class="col-md-9 mt-3 ">
+
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="top-part">
+                                    <div class="d-flex bd-highlight">
+                                        <div class="p-2 flex-grow-1 bd-highlight menu__part"><img src="img/menu.png"
+                                                alt="menu"></div>
+
+                                        <div class="p-2 bd-highlight login__part"><img class="login__img"
+                                                src="img/user.png" alt="menu">Login</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="row ">
+                            <div class="col-md-12 pt-2">
+                                <div class="add__categories_bottom">
+                                    <!-- Button trigger modal -->
+                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                        data-bs-target="#exampleModal">
+                                        Add user
+                                    </button>
+
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="row  mt-3">
+                            <div class="col-md-12">
+                                <div class="categories-tables">
+
+                                    <table class="table table-bordered bg-light table-responsiv">
+
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">#</th>
+                                                <th scope="col">Image</th>
+                                                <th scope="col">Category Name</th>
+                                                <th scope="col">Action</th>
+                                            </tr>
+                                        </thead>
+
+                                        <tbody>
+                                            @foreach ($category as $item)
+                                            <tr>
+                                                <th scope="row">{{$item->category_id}}</th>
+                                                <td class="image-categories"><img class="img__product"
+                                                        src="{{ url('storage/app/'.$item->category_image) }}" class="img-fluid" alt="..."></td>
+
+                                                <td>{{$item->category_name}}</td>
+                                                <td class="action-table">
+                                                    <div class=" dropstart">
+                                                        <button type="button" class="btn btn-success dropdown-toggle"
+                                                            data-bs-toggle="dropdown" aria-expanded="false">
+                                                            Action
+                                                        </button>
+                                                        <ul class="dropdown-menu">
+                                                            <li><a class="dropdown-item" wire:click = 'deletecategory({{$item->category_id}})'>Delete</a></li>
+                                                        </ul>
+                                                    </div>
+
+                                                </td>
+                                            </tr>
+
+
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
         <form method="POST" action="/adduser" enctype="multipart/form-data" >
             @csrf
             <label for="name">Name</label>
@@ -18,6 +214,23 @@
             <input type="password" name="password" id="">
             <input type="submit" value="Add">
         </form>
-    </div>
+
+    </section>
+
+    @livewireScripts
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+
+    <script src="js/script.js"></script>
+
+    <script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"
+        integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js"
+        integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous">
+    </script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
 </body>
+
 </html>
+
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\billprintcontroller;
-use App\Http\Controllers\categorycontroller;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\productcontroller;
 use App\Http\Controllers\UserController;
 use App\Models\Order;
 use Illuminate\Support\Facades\Route;
@@ -25,14 +23,16 @@ Route::get('/', function () {
 Route::get('/bill', function () {
     return view('billprint');
 });
+Route::get('/adduser',[UserController::class,'index']);
+
 
 Route::get('/login',[LoginController::class,'index'])->name('login');
 Route::post('/login',[LoginController::class,'authenticate']);
 
 
-
-// Route::view('/product', 'add__product');
-// Route::view('/product__list', 'product__list');
+Route::view('/categories', 'add__categories');
+Route::view('/product', 'add__product');
+Route::view('/product__list', 'product__list');
 
 
 
@@ -43,18 +43,8 @@ Route::view('/pos', 'pos');
 
 Route::middleware(['auth'])->group(function () {
 
-    Route::get('/categories',[categorycontroller::class,'index']);
-
-    Route::get('/product', [productcontroller::class,'showproduct']);
-    Route::get('/add-product', [productcontroller::class,'index']);
-    Route::post('/product', [productcontroller::class,'addproduct']);
-    Route::get('/product-delete/{id}', [productcontroller::class,'deleteproduct'])->name('product.delete');
-
-
 
     // userrout
-    Route::get('/adduser',[UserController::class,'index']);
     Route::post('/adduser',[UserController::class,'adduser'])->name('user.add');
 
 });
-
