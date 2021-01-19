53b73c01e05f4349553bc683521d997391457efd
bikashbist <bikas.bist@yahoo.com>
2021-01-19 11:16:11 +0545

    user list

--- resources/views/userlist.blade.php
+++ resources/views/userlist.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.master')
 
+
+
 @section('components')
+
     {{-- write code here --}}
     {{-- table head #,name,email,delete --}}
+    <div class="col-md-9 mt-3">
+
+
+    </div>
 @endsection
